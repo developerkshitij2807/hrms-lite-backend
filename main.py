ea5450a9bb53f03c72ef8bdd30e19100ab666004
Kshitij Dutt <kshtjdutt07@gmail.com>
2026-03-12 11:43:05 +0530

    enable cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routes.employee_routes import router as employee_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for assignment
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(employee_router)
 
